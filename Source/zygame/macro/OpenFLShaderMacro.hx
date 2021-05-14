ac90d5c532c6d3218143d74a8fc65945e9e21bb5
yutian.liu <939029835@qq.com>
2021-05-14 20:04:52 +0800

    初次OpenFLShader Haxe编写GLSL版本支持

--- Source/zygame/macro/OpenFLShaderMacro.hx
+++ Source/zygame/macro/OpenFLShaderMacro.hx
@@ -4,150 +4,195 @@ import haxe.macro.ExprTools;
 import haxe.macro.Expr;
 import haxe.macro.Context;
 
+/**
+ * 解析OpenFLShader的宏处理
+ */
 class OpenFLShaderMacro {
 	macro public static function buildShader():Array<Field> {
 		var fields = Context.getBuildFields();
 		var shader = "\n\r";
+		var maps:Map<String, String> = [];
 		for (field in fields) {
-			trace(field);
+			trace(field.kind.getName());
 			switch (field.kind.getName()) {
 				case "FVar":
 					// 变量定义
 					var type:ComplexType = cast field.kind.getParameters()[0];
-					var c = type.getParameters()[0].name;
-					shader += c.toLowerCase() + " " + field.name + ";";
+					var value = cast field.kind.getParameters()[1];
+					var c = type == null ? toExprType(value.expr) : type.getParameters()[0].name.toLowerCase();
+					if (value == null) {
+						shader += c + " " + field.name + ";\n\r";
+					} else {
+						shader += c + " " + field.name + "=" + toExprValue(value.expr) + ";\n\r";
+					}
 				case "FFun":
 					// 方法解析
+					maps.set(field.name, "");
 					shader += "\n\rvoid " + field.name + "(){\n\r";
 					var func:ExprDef = cast field.kind.getParameters()[0].expr.expr;
 					var array:Array<Dynamic> = func.getParameters()[0];
 					for (index => value in array) {
 						var expr:ExprDef = cast value.expr;
 						trace("方法解析：", expr.getName());
+						var line:String = "";
 						switch (expr.getName()) {
 							case "EField":
 							// 已定义对象赋值
-
 							case "EVars":
 								// 定义局部变量
-								var vars:Array<Dynamic> = expr.getParameters();
-								for (index => value in vars) {
-									trace("属性定义", value);
-									var array = cast(value, Array<Dynamic>);
-									for (index2 => value2 in array) {
-										shader += "  " + toGLSLType(value2.type, value2.expr.expr) + value2.name + "="
-											+ toGLSLValue(value2.type, value2.expr.expr) + ";\n\r";
-									}
-								}
+								var vars = expr.getParameters()[0];
+								var varvalue = vars[0].expr;
+								line += "  " + (vars[0].type != null ? toExprType(vars[0].type) : "???") + " " + vars[0].name;
+								trace(varvalue);
+								if (varvalue != null) line += "=" + toExprValue(varvalue.expr);
 							case "ECall":
 								// 调用方法
-								var args:Array<Dynamic> = cast expr.getParameters()[1];
-								var expr:ExprDef = cast expr.getParameters()[0].expr;
-								var pos:Constant = cast expr.getParameters()[0];
-								shader += "  " + pos.getParameters()[0] + "(" + toArgs(args) + ");\n\r";
+								var callname = toExprValue(expr.getParameters()[0].expr);
+								line += "  " + callname;
 							case "EBinop":
 								// 赋值
-								trace(expr.getParameters());
-								var binop = expr.getParameters()[0];
-								shader += "  " + toGLSLEBinop(binop, expr.getParameters()[1].expr, expr.getParameters()[2].expr) + ";\n\r";
+								var varname = toExprValue(expr);
+								line += "  " + varname;
+							case "EIf":
+								// If判断
+								line += "  " + toExprValue(expr);
+						}
+						if (line != "  null") {
+							maps.set(field.name, maps.get(field.name) + line + ";\n\r");
+							shader += line + ";\n\r";
 						}
 					}
 					shader += "\n\r}";
 			}
 		}
 		trace("\n\rGLSL脚本：\n\r" + shader);
+		// 创建new
+		var pos = Context.currentPos();
+		var fragment = "\n #pragma header \n void main(void){#pragma body\n" + maps.get("fragment") + "\n}";
+		trace("fragment=", fragment);
+		var newField = {
+			name: "new",
+			doc: null,
+			meta: [
+				{
+					name: ":glFragmentSource",
+					params: [macro $v{fragment}],
+					pos: pos
+				}
+			],
+			access: [APublic],
+			kind: FFun({
+				args: [],
+				ret: macro:Void,
+				expr: macro {super();}
+			}),
+			pos: pos
+		};
+		fields.push(newField);
 		return fields;
 	}
 
-	public static function toGLSLEBinop(binop:Dynamic, expr:ExprDef, value:ExprDef):String {
-		trace("toGLSLField", expr, "=", value);
-		if (binop == OpAssign) {
-			binop = "=";
-		}
-		if (expr.getName() == "EField") {
-			var f = expr.getParameters()[1];
-			expr = expr.getParameters()[0].expr;
-			expr = expr.getParameters()[0];
-			expr = expr.getParameters()[0];
-			return expr + "." + f + binop + toGLSLValue(null, value);
-		} else if (expr.getName() == "EConst") {
-			expr = expr.getParameters()[0];
-			expr = expr.getParameters()[0];
-			return expr + binop + toGLSLValue(null, value);
-		}
-		return "null";
-	}
-
-	public static function toGLSLValue(ctype:ComplexType, expr:Dynamic):String {
-		if (expr == null)
-			return "null";
-		var type:String = ctype == null ? null : ctype.getParameters()[0].name;
-		var def:ExprDef = cast expr;
-		var valueType = def.getName();
-		trace("valueType=", valueType);
-		if (valueType == "ECall") {
-			var args:Array<Dynamic> = cast def.getParameters()[1];
-			var def:ExprDef = cast def.getParameters()[0].expr;
-			var pos:Constant = cast def.getParameters()[0];
-			trace("args = ",args);
-			return pos.getParameters()[0] + "(" + toArgs(args) + ")";
-		} else if (valueType == "EField") {
-			var f = def.getParameters()[1];
-			def = def.getParameters()[0].expr;
-			def = def.getParameters()[0];
-			def = def.getParameters()[0];
-			return def + "." + f;
-		} else if (valueType == "ENew") {
-			// 新建变量，一般由类型组成
-			var c:String = def.getParameters()[0].name;
-			var args:Array<Dynamic> = def.getParameters()[1];
-			return c.toLowerCase() + "(" + toArgs(args) + ")";
-		} else {
-			def = def.getParameters()[0];
-			var value:String = def.getParameters()[0];
-			switch (type) {
-				case "Float":
-					if (value.indexOf(".") == -1)
-						value += ".";
-			}
-			return value;
-		}
-	}
-
-	public static function toGLSLType(ctype:ComplexType, expr:Dynamic):String {
-		if (ctype != null) {
-			return cast(ctype.getParameters()[0].name, String).toLowerCase() + " ";
-		}
-		var type = null;
-		var def:ExprDef = cast expr;
-		var data:Dynamic = def.getParameters()[0];
-		if (Std.isOfType(data, Constant)) {
-			type = cast(data, Constant).getName();
-		} else {
-			type = data.name;
-		}
-
+	/**
+	 * 解析Expr的层级类型
+	 * @return String
+	 */
+	public static function toExprType(expr:ExprDef):String {
+		var ret = "#invalidType#";
+		var type = expr.getName();
 		switch (type) {
-			case "CString":
-				throw "GLSL中不允许使用String类型";
-			case "CInt":
-				type = "int";
-			case "CFloat":
-				type = "float";
+			case "ENew", "TPath":
+				return expr.getParameters()[0].name.toLowerCase();
+			case "EConst":
+				expr = expr.getParameters()[0];
+				return expr.getName().substr(1).toLowerCase();
+			default:
+				throw "无法使用" + type + "建立类型关系";
 		}
-		return type + " ";
+		return ret;
 	}
 
-	public static function toArgs(data:Array<Dynamic>):String {
-		var newdata = [];
-		for (index => value in data) {
-			var expr:ExprDef = cast(value.expr, ExprDef).getParameters()[0];
-			trace("解析参数：", expr.getName(), expr.getParameters()[0]);
-			switch (expr.getName()) {
-				case "CInt":
-					newdata[index] = expr.getParameters()[0];
-			}
+	/**
+	 * 解析Expr的层级内容
+	 * @param expr 
+	 * @return String
+	 */
+	public static function toExprValue(expr:ExprDef, args:Array<Dynamic> = null):String {
+		var ret = "#invalidValue#";
+		var type = expr.getName();
+		switch (type) {
+			case "OpGt":
+				return ">";
+			case "OpLt":
+				return "<";
+			case "OpAssignOp":
+				return "*=";
+			case "OpAssign":
+				return "=";
+			case "OpAdd":
+				return "+";
+			case "OpDiv":
+				return "/";
+			case "OpMult":
+				return "*";
+			case "EIf":
+				var data = "";
+				var ifcontent = toExprValue(expr.getParameters()[0].expr);
+				var content = toExprValue(expr.getParameters()[1].expr);
+				var elsecontent = expr.getParameters()[2];
+				if (ifcontent != null) {
+					data += (args != null ? args[0] : "if") + "(" + ifcontent + "){" + content + ";}";
+				}
+				if (elsecontent != null) {
+					data += "else{" + toExprValue(elsecontent.expr, ["elseif"]) + ";}";
+				}
+				return data;
+			case "EField":
+				var value = toExprValue(expr.getParameters()[0].expr);
+				if (value == "this")
+					return expr.getParameters()[1];
+				if (value.indexOf("gl_openfl") == 0)
+					value = value.substr(3);
+				var ret = value + "." + expr.getParameters()[1];
+				if (ret == "super.fragment")
+					return null;
+				return ret;
+			case "EBinop":
+				var value1 = toExprValue(expr.getParameters()[1].expr);
+				var value2 = toExprValue(expr.getParameters()[2].expr);
+				var binop = toExprValue(expr.getParameters()[0]);
+				return value1 + binop + value2;
+			case "ECall":
+				var callName = toExprValue(expr.getParameters()[0].expr);
+				var args = expr.getParameters()[1];
+				return callName + "(" + toExprListValue(args) + ")";
+			case "ENew":
+				var ctype = toExprType(expr);
+				return ctype + "(" + toExprListValue(expr.getParameters()[1]) + ")";
+			case "EConst":
+				expr = expr.getParameters()[0];
+				var value:Dynamic = expr.getParameters()[0];
+				if (Std.isOfType(value, String)) {
+					if (value.indexOf("gl_openfl") == 0)
+						value = value.substr(3);
+				}
+				return value;
+			default:
+				throw "无法使用" + type + "建立值";
 		}
-		return newdata.join(",");
+		return ret;
+	}
+
+	/**
+	 * [Description]
+	 * @param Array<Dynamic> 
+	 * @return String
+	 */
+	public static function toExprListValue(array:Array<Dynamic>):String {
+		var ret:Array<String> = [];
+		for (index => value in array) {
+			trace(value.expr);
+			ret.push(toExprValue(value.expr));
+		}
+		return ret.join(",");
 	}
 }
--- Source/zygame/shader/engine/OpenFLShader.hx
+++ Source/zygame/shader/engine/OpenFLShader.hx
@@ -1,28 +1,44 @@
 package zygame.shader.engine;
 
+import openfl.display.DisplayObjectShader;
 import haxe.macro.Expr.Field;
 
 @:autoBuild(zygame.macro.OpenFLShaderMacro.buildShader())
-class OpenFLShader {
+class OpenFLShader extends DisplayObjectShader {
 	/** 
 	 * 纹理UV
 	 */
-	public var openfl_TextureCoordv:Vec2;
+	public var gl_openfl_TextureCoordv:Vec2;
 
 	/**
 	 * 纹理尺寸
 	 */
-	public var openfl_TextureSize:Vec2;
+	public var gl_openfl_TextureSize:Vec2;
+
+	/**
+	 * 纹理对象
+	 */
+	public var gl_openfl_Texture:Dynamic;
 
 	public var gl_FragColor:Vec4;
 
 	public var color:Vec4;
 
-	public function floor(f:Float):Dynamic{
+	public function floor(f:Float):Dynamic {
 		return f;
 	};
 
-	public function new() {}
+	public function texture2D(texture:Dynamic, vec2:Float):Vec4 {
+		return null;
+	}
+
+	public function fragment():Void {}
+
+	public function vertex():Void {}
+
+	public function new() {
+		super();
+	}
 }
 
 extern class Vec4 {

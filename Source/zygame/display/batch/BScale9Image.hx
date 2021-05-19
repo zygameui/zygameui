4f02f3d2489f505cac01255c268b323f9cef355a
yutian.liu <939029835@qq.com>
2021-05-19 22:35:13 +0800

    改进九宫格图的性能，不再使用多个瓦片组成渲染，而是用GLSL实现九宫格渲染。

--- Source/zygame/display/batch/BScale9Image.hx
+++ Source/zygame/display/batch/BScale9Image.hx
@@ -1,166 +1,52 @@
 package zygame.display.batch;
 
-import zygame.display.batch.BDisplayObjectContainer;
+import zygame.display.batch.BScale9Image2;
+import zygame.shader.Slice9Shader;
 import zygame.utils.load.Frame;
-import zygame.utils.load.TextureLoader;
 import zygame.display.batch.BImage;
-import openfl.display.Tile;
-import openfl.geom.Rectangle;
 
 /**
  * 九宫格批渲染支持，需要在精灵表中进行bindScale9后的frame才能正常使用
  */
-class BScale9Image extends BDisplayObjectContainer {
+class BScale9Image extends BImage {
+	public function new(frame:Frame = null) {
+		super();
+		this.width;
+		this.height;
+		// BScale9Image使用Slice9Shader着色器
+		this.shader = new zygame.shader.Slice9Shader(0, 0, 0, 0, 0, 0);
+		cast(this.shader, Slice9Shader).u_isFrameSprite.value = [true];
+		setFrame(frame);
+	}
 
-    public var curFrame:Frame;
+	override function setFrame(frame:Frame) {
+		super.setFrame(frame);
+		if (frame != null)
+			cast(this.shader, Slice9Shader).updateFrame(frame);
+	}
 
-    public var curParent:TextureAtlas;
+	override private function set_width(value:Float):Float {
+		super.set_width(value);
+		updateScale9();
+		return value;
+	}
 
-    private var _setWidth:Bool = false;
-    private var _width:Float = 0;
-    
-    private var _setHeight:Bool = false;
-    private var _height:Float = 0;
+	override private function set_height(value:Float):Float {
+		super.set_height(value);
+		updateScale9();
+		return value;
+	}
 
-    public function new(frame:Frame = null)
-    {
-        super();
-        this.width;
-        this.height;
-        setFrame(frame);
-    }
-
-    /**
-     * 设置新帧
-     * @param frame 
-     */
-    public function setFrame(frame:Frame):Void
-    {
-        if(frame == null || frame.scale9frames == null || frame == curFrame)
-            return;
-        super.set_id(frame.id);
-        curParent = frame.parent;
-        curFrame = frame;
-        //清空所有tiles
-        removeTiles();
-        //生成新的tiles
-        for(s9frame in frame.scale9frames)
-        {
-            var img:BImage = new BImage(s9frame);
-            this.addTile(img);
-        }
-        if(!_setWidth)
-            _width = frame.width;
-        if(!_setHeight)
-            _height = frame.height;
-        updateScale9();
-    }
-    
-    override private function set_width(value:Float):Float
-    {
-        _setWidth = true;
-        _width = value;
-        updateScale9();
-        return value;
-    }
-
-    override private function set_height(value:Float):Float
-    {
-        _setHeight = true;
-        _height = value;
-        updateScale9();
-        return value;
-    }
-
-     /**
-     * 更新刷新9宫格
-     */
-    private function updateScale9():Void
-    {
-        if(curFrame == null)
-            return;
-        for(i in 0...9)
-        {
-            var tile:Tile = this.getTileAt(i);
-            var left:Float = curFrame.scale9rect.x;
-            var right:Float = curFrame.width - curFrame.scale9rect.x - curFrame.scale9rect.width;
-            var bottom:Float = curFrame.height - curFrame.scale9rect.y - curFrame.scale9rect.height;
-            var top:Float = curFrame.scale9rect.y;
-            var cwidth:Float = _width - left - right;
-            var cheight:Float = _height - top - bottom;
-            var pscaleX:Float = cwidth / curFrame.scale9frames[i].width;
-            var pscaleY:Float = cheight / curFrame.scale9frames[i].height;
-            
-            switch(i)
-            {
-                case 0:
-                    //左上
-                case 1:
-                    //中上
-                    tile.x = left;
-                    tile.scaleX =  pscaleX;
-                case 2:
-                    //右上
-                    tile.x = _width - right;
-                case 3:
-                    //左中
-                    tile.y = top;
-                    tile.scaleY = pscaleY;
-                case 4:
-                    //居中
-                    tile.x = left;
-                    tile.y = top;
-                    tile.scaleX = pscaleX;
-                    tile.scaleY = pscaleY;
-                case 5:
-                    //右中
-                    tile.x = _width - right;
-                    tile.y = top;
-                    tile.scaleY = pscaleY;
-                case 6:
-                    //下左
-                    tile.y = _height - bottom;
-                case 7:
-                    //中下
-                    tile.x = left;
-                    tile.y = _height - bottom;
-                    tile.scaleX = pscaleX;
-                case 8:
-                    //右下
-                    tile.x = _width - right;
-                    tile.y = _height - bottom;
-            }
-        }
-    }
-
-    public function getClickBounds(tile:openfl.display.Tile):Rectangle
-    {
-        if(curFrame != null)
-        {
-            var rect:Rectangle = new Rectangle();
-            rect.setTo(-this.originX,-this.originY,this._width,this._height);
-            #if !flash
-            @:privateAccess rect.__transform (rect, matrix);
-            #end
-            // if(tile != null)
-            // {
-            //     rect.width *= tile.scaleX;
-            //     rect.height *= tile.scaleY;
-            // }
-            return rect;
-        }
-        return null;
-    }
-
-    override public function getBounds(tile:openfl.display.Tile):Rectangle
-    {
-        var rect:Rectangle = null;
-        if(this.parent == tile)
-            rect = getClickBounds(tile);
-        if(rect == null)
-            rect = super.getBounds(tile);
-        return rect;
-    }
-
-
+	/**
+	 * 更新刷新9宫格
+	 */
+	private function updateScale9():Void {
+		if (curFrame == null)
+			return;
+		var left:Float = curFrame.scale9rect.x;
+		var right:Float = curFrame.width - curFrame.scale9rect.x - curFrame.scale9rect.width;
+		var bottom:Float = curFrame.height - curFrame.scale9rect.y - curFrame.scale9rect.height;
+		var top:Float = curFrame.scale9rect.y;
+		cast(this.shader, Slice9Shader).updateArgs(left, top, bottom,right, this.width, this.height);
+	}
 }
---
+++ Source/zygame/shader/Slice9Shader.hx
@@ -0,0 +1,126 @@
+package zygame.shader;
+
+import zygame.utils.load.Frame;
+import glsl.GLSL;
+import VectorMath;
+import glsl.OpenFLShader;
+
+/**
+ * 九宫格渲染着色器
+ */
+class Slice9Shader extends OpenFLShader {
+	/**
+	 * 是否为精灵图对象
+	 */
+	@:uniform public var isFrameSprite:Bool;
+
+	/**
+	 * 精灵图的尺寸
+	 */
+	@:uniform public var frameSpriteRect:Vec4;
+
+	/**
+	 * 九宫格数据
+	 */
+	@:uniform public var s9d:Vec4;
+
+	/**
+	 * 显示对象尺寸
+	 */
+	@:uniform public var size:Vec2;
+
+	/**
+	 * 获取精灵图的Uv值
+	 * @return Vec2
+	 */
+	@:glsl public function getFrameCoodv():Vec2 {
+		return vec2(fract((gl_openfl_TextureSize.x * gl_openfl_TextureCoordv.x - frameSpriteRect.x) / frameSpriteRect.z),
+			fract((gl_openfl_TextureSize.y * gl_openfl_TextureCoordv.y - frameSpriteRect.y) / frameSpriteRect.w));
+	}
+
+	/**
+	 * 获取指定位置的UV图
+	 * @param x 
+	 * @param y 
+	 * @return Vec2
+	 */
+	@:glsl public function getUv(x:Float, y:Float):Vec2 {
+		if (isFrameSprite) {
+			return vec2(frameSpriteRect.x + x, frameSpriteRect.y + y) / gl_openfl_TextureSize;
+		}
+		return vec2(x / gl_openfl_TextureSize.x, y / gl_openfl_TextureSize.y);
+	}
+
+	override function fragment() {
+		super.fragment();
+		var uv:Vec2 = isFrameSprite ? getFrameCoodv() * size : gl_openfl_TextureCoordv * size;
+		var centerWidth:Float = size.x - s9d.x - s9d.y; // 中间宽度
+		var centerHeight:Float = size.y - s9d.z - s9d.w; // 中间高度
+		var centerSliceWidth:Float = (isFrameSprite ? frameSpriteRect.z : gl_openfl_TextureSize.x) - s9d.x - s9d.y; // 中间原图宽度
+		var centerSliceHeight:Float = (isFrameSprite ? frameSpriteRect.w : gl_openfl_TextureSize.y) - s9d.z - s9d.w; // 中间原图高度
+		gl_FragColor = vec4(0);
+		if (uv.x < s9d.x && uv.y < s9d.z) {
+			// 左上(ok)
+			// gl_FragColor = vec4(1,0,0,1);
+			gl_FragColor = texture2D(gl_openfl_Texture, getUv(uv.x, uv.y));
+		} else if (uv.x > size.x - s9d.y && uv.y < s9d.z) {
+			// 右上(ok)
+			// gl_FragColor = vec4(1, 0, 0, 1);
+			gl_FragColor = texture2D(gl_openfl_Texture, getUv(s9d.x + centerSliceWidth + uv.x - (size.x - s9d.y), uv.y));
+		} else if (uv.x < s9d.x && uv.y > size.y - s9d.w) {
+			// 左下
+			// gl_FragColor = vec4(1, 0, 0, 1);
+			gl_FragColor = texture2D(gl_openfl_Texture, getUv(uv.x, centerSliceHeight + s9d.z + uv.y - (size.y - s9d.w)));
+		} else if (uv.x > size.x - s9d.y && uv.y > size.y - s9d.w) {
+			// 右下
+			// gl_FragColor = vec4(1, 0, 0, 1);
+			gl_FragColor = texture2D(gl_openfl_Texture,
+				getUv(s9d.x + centerSliceWidth + uv.x - (size.x - s9d.y), centerSliceHeight + s9d.z + uv.y - (size.y - s9d.w)));
+		} else if (uv.y < s9d.z) {
+			// 上
+			gl_FragColor = texture2D(gl_openfl_Texture, getUv(s9d.x + (uv.y - s9d.z) / centerWidth * centerSliceWidth, uv.y));
+		} else if (uv.y > size.y - s9d.w) {
+			// 下
+			gl_FragColor = texture2D(gl_openfl_Texture,
+				getUv(s9d.x + (uv.y - s9d.z) / centerWidth * centerSliceWidth, centerSliceHeight + s9d.z + uv.y - (size.y - s9d.w)));
+		} else if (uv.x <= s9d.x) {
+			// 左(ok)
+			gl_FragColor = texture2D(gl_openfl_Texture, getUv(uv.x, s9d.z + (uv.y - s9d.z) / centerHeight * centerSliceHeight));
+		} else if (uv.x > size.x - s9d.y) {
+			// 右(ok)
+			gl_FragColor = texture2D(gl_openfl_Texture,
+				getUv(centerSliceWidth + s9d.x + uv.x - (size.x - s9d.y), s9d.z + (uv.y - s9d.z) / centerHeight * centerSliceHeight));
+		} else {
+			// 中间
+			gl_FragColor = texture2D(gl_openfl_Texture,
+				getUv(s9d.x + (uv.y - s9d.z) / centerWidth * centerSliceWidth, s9d.z + (uv.y - s9d.z) / centerHeight * centerSliceHeight));
+		}
+		// if (isFrameSprite) {
+		// gl_FragColor += texture2D(gl_openfl_Texture, gl_openfl_TextureCoordv);
+		// }
+	}
+
+	/**
+	 * @param left 左间距
+	 * @param top 上间距
+	 * @param right 右间距
+	 * @param bottom 下间距
+	 */
+	public function new(left:Float, top:Float, bottom:Float, right:Float, width:Float, height:Float) {
+		super();
+		// left/right/top/bottom
+		u_s9d.value = [left, right, top, bottom];
+		u_size.value = [width, height];
+	}
+
+	public function updateFrame(frame:Frame):Void {
+		u_frameSpriteRect.value = [frame.x, frame.y, frame.width, frame.height];
+		trace("frame=", u_frameSpriteRect.value);
+	}
+
+	public function updateArgs(left:Float, top:Float, bottom:Float, right:Float, width:Float, height:Float) {
+		trace(left, right, top, bottom, width, height);
+		u_s9d.value = [left, right, top, bottom];
+		u_size.value = [width, height];
+	}
+}

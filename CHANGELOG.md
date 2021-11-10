a6e9c827cfd4bcd5568b5394d5822113490fdecc
yutian.liu <939029835@qq.com>
2021-11-10 14:41:09 +0800

    改进流动布局支持

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -32,6 +32,7 @@
 - [改进] 改进`ZButton`的clickEvent改为可读。
 - [弃用] 不再支持OpenFL8，请直接使用OpenFL9。
 - [改进] 改进`ZBuilder.buildui`的align性能。
+- [新增] 新增`ZLabel.setFontLeading`设置文本的行距。
 
 ### 11.0.4
 - [新增] 新增`disable_dynamic_fps`可用于禁用动态FPS，部分平台可能不支持，需要禁用（例如vivo小游戏）。
--- Source/zygame/components/layout/FlowLayout.hx
+++ Source/zygame/components/layout/FlowLayout.hx
@@ -7,84 +7,95 @@ import openfl.display.DisplayObject;
 /**
  *  流水布局，需要指定ZBox的宽度或者高度，默认水平布局
  */
-class FlowLayout extends BaseLayout
-{
+class FlowLayout extends BaseLayout {
+	public static inline var HORIZONTAL:String = "horizontal";
 
-    public static inline var HORIZONTAL:String = "horizontal";
+	public static inline var VERTICAL:String = "vertical";
 
-    public static inline var VERTICAL:String = "vertical";
+	public var direction:String = HORIZONTAL;
 
-    public var direction:String = HORIZONTAL;
+	public var width:Float = 0;
 
-    override public function layout(box:ZBox):Void
-    {
-        if(direction == HORIZONTAL)
-            layoutHorizontal(box);
-        else
-            layoutVertical(box);
-    }
+	public var height:Float = 0;
 
-    private function layoutHorizontal(box:ZBox):Void
-    {
-        if(box.isAutoWidth())
-            throw "未对ZBox对象设置实际宽度，无法使用FlowLayout布局。";
-        var ix:Float = 0;
-        var max:Float = 0;
-        var iy:Float = 0;
-        var child:DisplayObject;
-        for(i in 0...box.childs.length)
-        {
-            child = box.childs[i];
-            if(ix + child.width > box.width)
-            {
-                ix = 0;
-                iy += max;
-                max = 0;
-                child.x = ix;
-                child.y = iy;
-            }
-            else
-            {
-                child.x = ix;
-                child.y = iy;
-                ix += child.width;
-                if(child.height > max)
-                    max = child.height;
-            }
-        
-            
-        }
-    }
+	/**
+	 * 上下左右的间隔
+	 */
+	public var gap:Float = 0;
 
-    private function layoutVertical(box:ZBox):Void
-    {
-        if(box.isAutoHeight())
-            throw "未对ZBox对象设置实际高度，无法使用FlowLayout布局。";
-        var ix:Float = 0;
-        var max:Float = 0;
-        var iy:Float = 0;
-        var child:DisplayObject;
-        for(i in 0...box.childs.length)
-        {
-            child = box.childs[i];
-            if(iy + child.height > box.height)
-            {
-                iy = 0;
-                ix += max;
-                max = 0;
-                child.x = ix;
-                child.y = iy;
-            }
-            else
-            {
-                child.x = ix;
-                child.y = iy;
-                iy += child.height;
-                if(child.width > max)
-                    max = child.width;
-            }
-            
-        }
-    }
+	/**
+	 * 左右的间隔
+	 */
+	public var gapX:Float = 0;	
 
+	/**
+	 * 上下的间隔
+	 */
+	public var gapY:Float = 0;
+
+	/**
+	 * 流动布局，需要指定宽高，如果不指定，则以ZBox的宽高处理
+	 * @param width 
+	 * @param height 
+	 */
+	public function new(width:Float = 0, height:Float = 0) {
+		super();
+		this.width = width;
+		this.height = height;
+	}
+
+	override public function layout(box:ZBox):Void {
+		if (direction == HORIZONTAL)
+			layoutHorizontal(box);
+		else
+			layoutVertical(box);
+	}
+
+	private function layoutHorizontal(box:ZBox):Void {
+		var ix:Float = 0;
+		var max:Float = 0;
+		var iy:Float = 0;
+		var child:DisplayObject;
+		var targetWidth = width == 0 ? box.width : width;
+		for (i in 0...box.childs.length) {
+			child = box.childs[i];
+			if (ix + child.width > targetWidth) {
+				ix = 0;
+				iy += max + (gap + gapY);
+				max = 0;
+				child.x = ix;
+				child.y = iy;
+			} else {
+				child.x = ix;
+				child.y = iy;
+				ix += child.width + (gap + gapX);
+				if (child.height > max)
+					max = child.height;
+			}
+		}
+	}
+
+	private function layoutVertical(box:ZBox):Void {
+		var ix:Float = 0;
+		var max:Float = 0;
+		var iy:Float = 0;
+		var child:DisplayObject;
+		var targetHeight = height == 0 ? box.height : height;
+		for (i in 0...box.childs.length) {
+			child = box.childs[i];
+			if (iy + child.height > targetHeight) {
+				iy = 0;
+				ix += max + (gap + gapX);
+				max = 0;
+				child.x = ix;
+				child.y = iy;
+			} else {
+				child.x = ix;
+				child.y = iy;
+				iy += child.height + (gap + gapY);
+				if (child.width > max)
+					max = child.width;
+			}
+		}
+	}
 }

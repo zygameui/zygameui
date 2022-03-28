61aed89485da89623d139914967bd40efa56f8f2
Rainy <939029835@qq.com>
2022-03-28 10:53:49 +0800

    新增一种简易的防CE修改的变量

--- Source/zygame/utils/CEFloat.hx
+++ Source/zygame/utils/CEFloat.hx
@@ -1,29 +1,118 @@
 package zygame.utils;
 
 /**
- * 可以防止内存修改的CEFloat值
+ * 一种防止内存修改器进行修改的简易方案
  */
-class CEFloat {
-	private var _value:String = null;
+abstract CEFloat(CEData) to CEData from CEData {
+	/**
+	 * 构造函数
+	 * @param value 
+	 */
+	inline public function new(value:Float) {
+		this = new CEData(value);
+	}
 
-	private var _value2:Float = 0;
+	/**
+	 * 接收Float值
+	 * @param s 
+	 */
+	@:from
+	static public function fromFloat(s:Float) {
+		return new CEFloat(s);
+	}
 
-	public function new(value:Float = 0) {
+	/**
+	 * 接收String值
+	 * @param s 
+	 */
+	@:from
+	static public function fromString(s:String) {
+		return new CEFloat(Std.parseFloat(s));
+	}
+
+	/**
+	 * 转为Float
+	 * @return Float
+	 */
+	@:to public function toFloat():Float {
+		return this.value;
+	}
+
+	/**
+	 * 转为String
+	 * @return String
+	 */
+	@:to public function toString():String {
+		return this.toString();
+	}
+
+	/**
+	 * 加法运算
+	 * @param value 
+	 * @return Float
+	 */
+	@:op(A + B) public function add(value:Float):Float {
+		return this.value += value;
+	}
+
+	/**
+	 * 减法运算
+	 * @param value 
+	 * @return Float
+	 */
+	@:op(A - B) public function jian(value:Float):Float {
+		return this.value -= value;
+	}
+
+	/**
+	 * 乘法运算
+	 * @param value 
+	 * @return Float
+	 */
+	@:op(A * B) public function mul(value:Float):Float {
+		return this.value *= value;
+	}
+
+	/**
+	 * 除法运算
+	 * @param value 
+	 * @return Float
+	 */
+	@:op(A / B) public function div(value:Float):Float {
+		return this.value /= value;
+	}
+}
+
+/**
+ * 一种防止内存修改器进行修改的简易方案
+ */
+class CEData {
+	/**
+	 * 签名值
+	 */
+	public var sign:String = null;
+
+	public function new(value:Float) {
 		this.value = value;
 	}
 
 	public var value(get, set):Float;
 
-	private function set_value(f:Float):Float {
-		_value2 = f;
-		_value = Lib.ceEncode(f);
-		return f;
+	public var values:Array<String> = [];
+
+	public function toString():String {
+		return Std.string(value);
 	}
 
-	private function get_value():Float {
-		if (_value == null)
-			return 0;
-		else
-			return Lib.ceDecode(_value);
+	function get_value():Float {
+		if (sign != values.join(""))
+			throw "游戏内存被修改";
+		return Std.parseFloat(sign);
+	}
+
+	function set_value(value:Float):Float {
+		sign = Std.string(value);
+		values = sign.split("");
+		return value;
 	}
 }

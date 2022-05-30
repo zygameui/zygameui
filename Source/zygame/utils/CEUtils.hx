cdf3ea8348baf7e66b83e870bb7ce32c9df597fc
Rainy <939029835@qq.com>
2022-05-30 17:02:16 +0800

    Update CEUtils.hx

--- Source/zygame/utils/CEUtils.hx
+++ Source/zygame/utils/CEUtils.hx
@@ -1,8 +1,10 @@
 package zygame.utils;
 
+#if crypto
 import haxe.crypto.padding.Padding;
 import haxe.crypto.mode.Mode;
 import haxe.crypto.Aes;
+#end
 import haxe.io.Bytes;
 import haxe.crypto.Base64;
 
@@ -25,10 +27,12 @@ class CEUtils {
 		return Base64.decode(data).toString();
 	}
 
+	#if crypto
 	/**
 	 * AES加密源
 	 */
 	public static var aes:Aes;
+	#end
 
 	/**
 	 * 左右对应key
@@ -36,7 +40,9 @@ class CEUtils {
 	 * @param rightKey 16位秘钥
 	 */
 	public static function initAesKey(leftKey:String, rightKey:String):Void {
+		#if crypto
 		aes = new Aes(Bytes.ofString(leftKey), Bytes.ofString(rightKey));
+		#end
 	}
 
 	/**
@@ -46,12 +52,16 @@ class CEUtils {
 	 * @return String
 	 */
 	public static function aesEncode(data:String):String {
+		#if crypto
 		if (aes == null)
 			throw "需要先使用CEUtils.initAesKey进行初始化";
 		var bytes = Bytes.ofString(data);
 		var encryptdata = aes.encrypt(Mode.CBC, bytes, Padding.PKCS7);
 		var str = Base64.encode(encryptdata, false);
 		return str;
+		#else
+		throw "使用CEUtils.aesEncode时，需要引入crypto库";
+		#end
 	}
 
 	/**
@@ -61,10 +71,14 @@ class CEUtils {
 	 * @return String
 	 */
 	public static function aesDecode(data:String):String {
+		#if crypto
 		if (aes == null)
 			throw "需要先使用CEUtils.initAesKey进行初始化";
 		var bytes = Base64.decode(data, false);
 		var encryptdata = aes.decrypt(Mode.CBC, bytes, Padding.PKCS7);
 		return encryptdata.toString();
+		#else
+		throw "使用CEUtils.aesDecode时，需要引入crypto库";
+		#end
 	}
 }

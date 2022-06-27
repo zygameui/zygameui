7392e553e1cae47abbb3eb0b5334f2d9c5961404
Rainy <939029835@qq.com>
2022-06-27 16:41:44 +0800

    为ListView新增ctrlKey自动选中、最大可选择数量等支持

--- Source/zygame/feathersui/FListView.hx
+++ Source/zygame/feathersui/FListView.hx
@@ -4,8 +4,25 @@ package zygame.feathersui;
 import feathers.controls.ListView;
 
 class FListView extends ListView {
+	/**
+	 * 是否一直使ctrlKey为true，当处于这种状态时，可点击多选
+	 */
+	public var autoCtrlKey:Bool = false;
+
+	/**
+	 * 最大可选择数量，为0时可无限选择
+	 */
+	public var maxSelectCounts:Int = 0;
+
 	public function new() {
 		super();
+	}
+
+	override function handleSelectionChange(item:Dynamic, index:Int, ctrlKey:Bool, shiftKey:Bool) {
+		if (autoCtrlKey)
+			ctrlKey = true;
+		if (maxSelectCounts <= 0 || maxSelectCounts > this.selectedItems.length || this.selectedItems.indexOf(item) != -1)
+			super.handleSelectionChange(item, index, ctrlKey, shiftKey);
 	}
 }
 #end

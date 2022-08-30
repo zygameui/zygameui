c91a38de6a8c8f5befde5e6dc751346b7d4696b3
Rainy <939029835@qq.com>
2022-08-30 17:14:22 +0800

    改进微信小游戏的储存支持

---
+++ Source/zygame/net/SharedObject.hx
@@ -0,0 +1,144 @@
+package zygame.net;
+
+import haxe.io.Bytes;
+import haxe.io.Path;
+import haxe.Serializer;
+import haxe.Unserializer;
+import openfl.errors.Error;
+import openfl.events.EventDispatcher;
+import openfl.utils.Object;
+#if lime
+import lime.app.Application;
+import lime.system.System;
+#end
+#if (js && html5)
+import js.Browser;
+#end
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
+import openfl.net.SharedObjectFlushStatus;
+
+/**
+ * 这是一个改进的SharedObject，在微信小游戏上，储存序列改进
+ */
+class SharedObject extends openfl.net.SharedObject {
+	/**
+	 * 获取一个数据共享对象
+	 * @param name 
+	 * @return SharedObject
+	 */
+	public static function getLocal(name:String, localPath:String = null, secure:Bool = false /* note: unsupported**/):openfl.net.SharedObject {
+		#if weixin
+		// 微信小游戏改进
+		var illegalValues = [" ", "~", "%", "&", "\\", ";", ":", "\"", "'", ",", "<", ">", "?", "#"];
+		var allowed = true;
+		if (name == null || name == "") {
+			allowed = false;
+		} else {
+			for (value in illegalValues) {
+				if (name.indexOf(value) > -1) {
+					allowed = false;
+					break;
+				}
+			}
+		}
+		if (!allowed) {
+			throw new Error("Error #2134: Cannot create SharedObject.");
+			return null;
+		}
+		if (@:privateAccess openfl.net.SharedObject.__sharedObjects == null) {
+			@:privateAccess openfl.net.SharedObject.__sharedObjects = new Map();
+			// Lib.application.onExit.add (application_onExit);
+			#if lime
+			if (Application.current != null) {
+				Application.current.onExit.add(@:privateAccess openfl.net.SharedObject.application_onExit);
+			}
+			#end
+		}
+		var id = localPath + "/" + name;
+		if (! @:privateAccess openfl.net.SharedObject.__sharedObjects.exists(id)) {
+			var encodedData = null;
+			try {
+				#if (js && html5)
+				var storage = Browser.getLocalStorage();
+				if (localPath == null) {
+					// Check old default path, first
+					if (storage != null) {
+						encodedData = storage.getItem(Browser.window.location.href + ":" + name);
+						storage.removeItem(Browser.window.location.href + ":" + name);
+					}
+					localPath = Browser.window.location.pathname;
+				}
+				if (storage != null && encodedData == null) {
+					encodedData = storage.getItem(localPath + ":" + name);
+				}
+				#else
+				if (localPath == null)
+					localPath = "";
+				var path = __getPath(localPath, name);
+				if (FileSystem.exists(path)) {
+					encodedData = File.getContent(path);
+				}
+				#end
+			} catch (e:Dynamic) {}
+			var sharedObject = new SharedObject();
+			sharedObject.data = {};
+			sharedObject.__localPath = localPath;
+			sharedObject.__name = name;
+			if (encodedData != null && encodedData != "") {
+				try {
+					if (encodedData is String) {
+						var unserializer = new Unserializer(encodedData);
+						unserializer.setResolver(cast {resolveEnum: Type.resolveEnum, resolveClass: @:privateAccess openfl.net.SharedObject.__resolveClass});
+						sharedObject.data = unserializer.unserialize();
+					} else {
+						// 这是微信小游戏的改进，不再使用Haxe的序列处理，直接使用Json序列
+						sharedObject.data = encodedData;
+					}
+				} catch (e:Dynamic) {}
+			}
+			@:privateAccess openfl.net.SharedObject.__sharedObjects.set(id, sharedObject);
+		}
+		return @:privateAccess openfl.net.SharedObject.__sharedObjects.get(id);
+		#else
+		return openfl.net.SharedObject.getLocal(name);
+		#end
+	}
+
+	override function flush(minDiskSpace:Int = 0):SharedObjectFlushStatus {
+		#if weixin
+		if (Reflect.fields(data).length == 0) {
+			return SharedObjectFlushStatus.FLUSHED;
+		}
+		// 微信小游戏改进，这里不做Haxe序列化，直接通过Json序列化
+		var encodedData = data;
+		try {
+			#if (js && html5)
+			var storage = Browser.getLocalStorage();
+			if (storage != null) {
+				storage.removeItem(__localPath + ":" + __name);
+				storage.setItem(__localPath + ":" + __name, encodedData);
+			}
+			#else
+			var path = __getPath(__localPath, __name);
+			var directory = Path.directory(path);
+
+			if (!FileSystem.exists(directory)) {
+				__mkdir(directory);
+			}
+
+			var output = File.write(path, false);
+			output.writeString(encodedData);
+			output.close();
+			#end
+		} catch (e:Dynamic) {
+			return SharedObjectFlushStatus.PENDING;
+		}
+		return SharedObjectFlushStatus.FLUSHED;
+		#else
+		return super.flush(minDiskSpace);
+		#end
+	}
+}

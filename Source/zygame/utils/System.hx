2616f47dad508f0c20f56e33266bc78f37af4b3a
Rainy <939029835@qq.com>
2023-11-07 09:43:01 +0800

    新增抖动API

--- Source/zygame/utils/System.hx
+++ Source/zygame/utils/System.hx
@@ -1,5 +1,6 @@
 package zygame.utils;
 
+import motion.Actuate;
 import zygame.macro.ZMacroUtils;
 
 /**
@@ -10,4 +11,13 @@ class System {
 	 * 建造时间
 	 */
 	public static var buildTime:String = ZMacroUtils.buildDateTime();
+
+	/**
+	 * 抖动效果，请注意，需要存在支持的平台才会支持，例如`android`/`ios`等原生平台，通常都会直接支持。当不支持时，调用该接口不会产生任何效果。
+	 */
+	public static function virrate(time:Float = 0.15):Void {
+		#if wechat
+		untyped wx.vibrateShort();
+		#end
+	}
 }

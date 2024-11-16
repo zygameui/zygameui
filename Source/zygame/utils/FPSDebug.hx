3f2ead56127876f3403d32ed7b57574ba7946b89
Rainy <939029835@qq.com>
2024-11-16 17:12:33 +0800

    新增GPU内存，并优化TextFieldCOntextBitmapData渲染丢失的问题

--- Source/zygame/utils/FPSDebug.hx
+++ Source/zygame/utils/FPSDebug.hx
@@ -82,24 +82,25 @@ class FPSDebug extends ZBox {
 		var mem:Float = Math.round(System.totalMemory / 1024 / 1024 * 100) / 100;
 		if (mem > memPeak)
 			memPeak = mem;
-		#if (gl_stats)
-		if (Context3DStats.totalDrawCalls() != 0)
-			_curDrawCall = Context3DStats.totalDrawCalls();
-		#end
 		if (visible) {
 			if (debugMsg != null)
 				_text.dataProvider = debugMsg;
 			else {
+				var gpumem:Float = Math.round(Start.current.stage.context3D.totalGPUMemory / 1024 / 1024 * 100) / 100;
+				#if (gl_stats)
+				if (Context3DStats.totalDrawCalls() != 0)
+					_curDrawCall = Context3DStats.totalDrawCalls();
+				#end
 				_alldt += zygame.core.Start.current.getIntervalTime();
 				_allcpu += zygame.core.Start.current.getCPUTime();
 				_alltimes++;
 				var fps = Std.int(_alldt / _alltimes);
 				// #if html5 + "\nTexture:" + zygame.core.Start.TEXTURE_COUNT #end 无意义，释放会由GC处理
 				var msg = "CPU:" + getCpu() + "_" + zygame.core.Start.current.getCPUTime() + "\nMODE:" + Lib.getRenderMode() + "\nMEM:" + mem
-					+ "MB\nMaxMEN:" + memPeak + "MB\nUPDATES:" + zygame.core.Start.current.getUpdateLength() + "\nSUPDATES:" + SpineManager.count()
-					+ "\nS_RUNING:" + SpineManager.playingCount + "\nFPS:" + Std.int(16 / fps * 60) + "_" + Start.current.renderFps + "_"
-					+ zygame.core.Start.current.stage.frameRate + "\nDrawCalls:" + (_curDrawCall - 2) + "\nScale:" + Start.currentScale + "\ntelemetry:"
-					+ Telemetry.connected;
+					+ "MB\nGPUMEM:" + gpumem + "\nMaxMEN:" + memPeak + "MB\nUPDATES:" + zygame.core.Start.current.getUpdateLength() + "\nSUPDATES:"
+					+ SpineManager.count() + "\nS_RUNING:" + SpineManager.playingCount + "\nFPS:" + Std.int(16 / fps * 60) + "_" + Start.current.renderFps
+					+ "_" + zygame.core.Start.current.stage.frameRate + "\nDrawCalls:" + (_curDrawCall - 2) + "\nScale:" + Start.currentScale
+					+ "\ntelemetry:" + Telemetry.connected;
 				#if (android && sxk_game_sdk)
 				var memoryInfo = v4.android.JNIBinding.getInstance().getApplictionMemory();
 				if (memoryInfo != null) {
